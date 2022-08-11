a1e3bebb25988e8c26b4154071a79464b812696f
bailiff-argus <dmartynenko64@gmail.com>
2022-08-11 20:57:04 +0300

    added app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+def surroundString(s: str) -> str:
+    return f"SUR({s})ROUND"
+
+
+def main() -> None:
+    string: str = "something"
+    print(f"raw: {string}")
+    print(f"surrounded: {surroundString(string)}")
+
+if __name__ == "__main__":
+    main()
